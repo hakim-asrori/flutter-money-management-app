247ad94239250a0732762666f0e3a570813b6991
Hakim Asrori <77718626+hakim-asrori@users.noreply.github.com>
2022-03-25 14:29:22 +0700

    Add files via upload

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,96 +52,132 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             SizedBox(height: 25),
             Padding(
-              padding: EdgeInsets.only(left: 16, bottom: 16,),
+              padding: EdgeInsets.only(
+                left: 16,
+                bottom: 16,
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text('Good Morning', style: GoogleFonts.inter(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: kBlackColor
-                  ),), 
-                  Text('Amanda Alex', style: GoogleFonts.inter(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w700,
-                    color: kBlackColor
-                  ))
-                  ],
+                  Text(
+                    'Good Morning',
+                    style: GoogleFonts.inter(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: kBlackColor),
+                  ),
+                  Text('Amanda Alex',
+                      style: GoogleFonts.inter(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                          color: kBlackColor))
+                ],
               ),
             ),
-
             Container(
               height: 199,
               child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                padding: EdgeInsets.only(left: 16, right: 6),
-                itemCount: cards.length,
-                itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.only(right: 10),
-                  height: 199,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(28),
-                    color: Color(int.parse(cards[index].cardBackground)),
-                  ),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned(
-                        child: SvgPicture.asset(cards[index].cardElementTop)
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.only(left: 16, right: 6),
+                  itemCount: cards.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                        margin: EdgeInsets.only(right: 10),
+                        height: 199,
+                        width: 344,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          color: Color(int.parse(cards[index].cardBackground)),
+                        ),
+                        child: Stack(children: <Widget>[
+                          Positioned(
+                              child: SvgPicture.asset(
+                                  cards[index].cardElementTop)),
+                          Positioned(
+                              right: 0,
+                              bottom: 0,
+                              child: SvgPicture.asset(
+                                  cards[index].cardElementBottom)),
+                          Positioned(
+                            left: 29,
+                            top: 48,
+                            child: Text(
+                              'CARD NUMBER',
+                              style: GoogleFonts.inter(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: kWhiteColor),
+                            ),
+                          ),
+                          Positioned(
+                            left: 29,
+                            top: 68,
+                            child: Text(
+                              cards[index].cardNumber,
+                              style: GoogleFonts.inter(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                  color: kWhiteColor),
+                            ),
+                          ),
+                          Positioned(
+                              right: 21,
+                              top: 35,
+                              child: Image.asset(cards[index].cardType)),
+                          Positioned(
+                              left: 29,
+                              bottom: 45,
+                              child: Text('CARD HOLDERNAME',
+                                  style: GoogleFonts.inter(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                      color: kWhiteColor))),
+                          Positioned(
+                              left: 29,
+                              bottom: 21,
+                              child: Text(cards[index].user,
+                                  style: GoogleFonts.inter(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700,
+                                      color: kWhiteColor))),
+                          Positioned(
+                              left: 209,
+                              bottom: 45,
+                              child: Text('EXPIRY DATE',
+                                  style: GoogleFonts.inter(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                      color: kWhiteColor))),
+                          Positioned(
+                              left: 209,
+                              bottom: 21,
+                              child: Text(cards[index].cardExpired,
+                                  style: GoogleFonts.inter(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700,
+                                      color: kWhiteColor))),
+                        ]));
+                  }),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, bottom: 13, top: 29),
+              child: Row(
+                children: <Widget>[
+                  Text('Operations'),
+                  Row(
+                      children: map<Widget>(datas, (index, selected) {
+                    return Container(
+                      alignment: Alignment.centerLeft,
+                      height: 9,
+                      width: 9,
+                      decoration: BoxDecoration(
+                        shape: 
                       ),
-                      Positioned(
-                        right: 0,
-                        bottom: 0,
-                        child: SvgPicture.asset(cards[index].cardElementBottom)
-                      ),
-                      Positioned(
-                        left: 29,
-                        top: 48,
-                        child: Text('CARD NUMBER', style: GoogleFonts.inter(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: kWhiteColor
-                        ),),
-                      ),
-                      Positioned(
-                        left: 29,
-                        top: 68,
-                        child: Text(cards[index].cardNumber, style: GoogleFonts.inter(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                          color: kWhiteColor
-                        ),),
-                      ),
-                      Positioned(
-                        right: 21,
-                        top: 35,
-                        child: Image.asset(cards[index].cardType)
-                      ),
-                      Positioned(
-                        left: 29,
-                        bottom: 45,
-                        child: Text('CARD HOLDERNAME', style: GoogleFonts.inter(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: kWhiteColor
-                        ))
-                      ),
-                      Positioned(
-                        left: 29,
-                        bottom: 21,
-                        child: Text(cards[index].user, style: GoogleFonts.inter(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                          color: kWhiteColor
-                        ))
-                      ),
-                    ]
-                  )
-                );
-              }),
-            )
-
+                    );
+                  }))
+                ],
+              ),
+            ),
           ],
         ),
       ),
